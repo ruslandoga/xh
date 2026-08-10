b6d09c2ade68099850027bb8350a4371ac88199e
ruslandoga <ruslandoga+gh@icloud.com>
2026-08-10 19:17:25 +0300

    Bootstrap Xh library (#14)

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule Xh.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/ruslandoga/xh"
+  @version "0.1.0"
+
   def project do
     [
       app: :xh,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      description: "ClickHouse HTTP client for Elixir",
+      package: package(),
+      source_url: @source_url,
       dialyzer: [plt_local_path: "plts", plt_core_path: "plts"]
     ]
   end
@@ -22,9 +28,17 @@ defmodule Xh.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:mint, "~> 1.8"},
+      {:nimble_pool, "~> 1.1"},
+      {:nimble_options, "~> 1.1"},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 end
--- test/xh_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule XhTest do
-  use ExUnit.Case
-  doctest Xh
-
-  test "greets the world" do
-    assert Xh.hello() == :world
-  end
-end
