e0b92db01edec8558a16d0209176b6b9e5a5a0b5
ruslandoga <ruslandoga+gh@icloud.com>
2026-08-10 18:39:04 +0300

    Configure Dialyzer

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Xh.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_local_path: "plts", plt_core_path: "plts"]
     ]
   end
 
@@ -21,6 +22,7 @@ defmodule Xh.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
