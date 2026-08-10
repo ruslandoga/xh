e663d03a992bf71c6b326cd3e8d559fd821a3a61
ruslandoga <ruslandoga+gh@icloud.com>
2026-08-10 18:34:21 +0300

    Support Elixir 1.18

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Xh.MixProject do
     [
       app: :xh,
       version: "0.1.0",
-      elixir: "~> 1.20",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
